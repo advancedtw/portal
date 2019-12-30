bd6ec7816607c86890d403eb25ec78c4ef2f195c
Rody Davis <rody@rive.app>
2019-12-30 15:48:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# portal
+
+A flutter plugin to build a widget outside of it's current tree. Similar to [React Portals](https://reactjs.org/docs/portals.html).
+
+## Example
+
+```dart
+import 'package:flutter/material.dart';
+
+import 'package:portal/portal.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PortalProvider(
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _provider = PortalProvider.of(context);
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('React Portal Example'),
+      ),
+      body: ListView.separated(
+        itemCount: 500,
+        separatorBuilder: (_, index) =>
+            Container(height: 2, color: Colors.grey),
+        itemBuilder: (_, index) {
+          final _key = ValueKey(index);
+
+          return Builder(
+            builder: (context) => ListTile(
+              title: Text('Test Portal: $index'),
+              onTap: () {
+                final _portal = ReactPortal(
+                    key: _key,
+                    builder: (_) => Card(
+                          elevation: 5,
+                          child: InkWell(
+                            onTap: () {
+                              _provider.removeKey(_key);
+                            },
+                            child: Container(
+                              width: 200,
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text('Info about the list item..'),
+                                    subtitle: Text(index.toString()),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ));
+                _portal.show(context);
+              },
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.visibility),
+        onPressed: () {
+          final _key = ValueKey('popup');
+          final _portal = ReactPortal(
+              key: _key,
+              alignment: Alignment.center,
+              width: 200,
+              height: 500,
+              builder: (_) => Card(
+                    elevation: 20.0,
+                    child: Container(
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned.fill(
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  title: Text('Popu Up Card'),
+                                  subtitle: Text('Sub List Example'),
+                                ),
+                                Expanded(
+                                  child: ListView.builder(
+                                    itemCount: 100,
+                                    itemBuilder: (_, index) => ListTile(
+                                      title: Text(index.toString()),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            child: IconButton(
+                              icon: Icon(Icons.close),
+                              onPressed: () {
+                                _provider.removeKey(_key);
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ));
+          _portal.show(context);
+        },
+      ),
+    );
+  }
+}
+
+```
---
+++ lib/portal.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class ReactPortal {
+  final Key key;
+  final bool asWideAsParent, asTallAsParent;
+  double left;
+  double bottom;
+  double right;
+  double top;
+  final double width, height;
+  final WidgetBuilder builder;
+  final RenderObject ancestor;
+  final bool maintainState;
+  final bool opaque;
+  final Alignment alignment;
+
+  ReactPortal({
+    this.key,
+    this.asWideAsParent = false,
+    this.asTallAsParent = false,
+    this.bottom,
+    this.left = 0,
+    this.top = 0,
+    this.right,
+    this.width,
+    this.height,
+    this.alignment,
+    @required this.builder,
+    this.ancestor,
+    this.maintainState = false,
+    this.opaque = false,
+  });
+
+  OverlayEntry show(
+    BuildContext context, {
+    OverlayEntry above,
+    OverlayEntry below,
+  }) {
+    OverlayEntry _entry = buildEntry(context);
+    final _portal = PortalProvider.of(context);
+    if (_portal != null) {
+      _portal.addEntry(context, _entry, key: key, above: above, below: below);
+    } else {
+      final _overlay = Overlay.of(context);
+      _overlay.insert(_entry, above: above, below: below);
+    }
+    return _entry;
+  }
+
+  OverlayEntry buildEntry(BuildContext context) {
+    RenderBox renderBox = context.findRenderObject();
+    final size = renderBox.size;
+    final offset = renderBox.localToGlobal(Offset.zero, ancestor: ancestor);
+    final _entry = OverlayEntry(
+      opaque: opaque,
+      maintainState: maintainState,
+      builder: (context) {
+        final _child = builder(context);
+        final _isMaterial = _child is Material || _child is Card;
+        final _size = Size(asWideAsParent ? size.width : width,
+            asTallAsParent ? size.height : height);
+        if (alignment != null) {
+          return Positioned.fill(
+            child: Container(
+              alignment: alignment,
+              width: _size.width,
+              height: _size.height,
+              child: _buildChild(_child, _isMaterial, _size),
+            ),
+          );
+        }
+        return Positioned(
+          left: left == null ? null : offset.dx + left,
+          right: right == null ? null : offset.dx + right,
+          bottom: bottom == null ? null : offset.dy + bottom,
+          top: top == null ? null : offset.dy + top,
+          width: _size.width,
+          height: _size.height,
+          child: _buildChild(_child, _isMaterial, _size),
+        );
+      },
+    );
+    return _entry;
+  }
+
+  Widget _buildChild(Widget child, bool isMaterial, Size size) {
+    Widget _child = child;
+    if (size?.width != null && size?.height != null) {
+      _child = SizedBox.fromSize(child: child, size: size);
+    }
+    if (isMaterial) {
+      return _child;
+    }
+    return Material(
+      color: Colors.transparent,
+      child: _child,
+    );
+  }
+}
+
+class PortalProvider extends InheritedWidget {
+  PortalProvider({
+    @required Widget child,
+  }) : _child = child;
+
+  @override
+  Widget get child => _child;
+
+  final Widget _child;
+  final Map<Key, OverlayEntry> _entries = {};
+  List<OverlayEntry> get entries => _entries.values;
+
+  void updateEntries(
+    BuildContext context,
+    List<OverlayEntry> entries, {
+    OverlayEntry below,
+    OverlayEntry above,
+  }) {
+    final _overlay = Overlay.of(context);
+    _overlay.rearrange(entries, above: above, below: below);
+    _entries.clear();
+    if (entries != null && entries.isNotEmpty) {
+      for (var entry in entries) {
+        _entries[ValueKey(entry.hashCode)] = entry;
+      }
+    }
+  }
+
+  void addEntry(
+    BuildContext context,
+    OverlayEntry entry, {
+    Key key,
+    OverlayEntry below,
+    OverlayEntry above,
+  }) {
+    final _overlay = Overlay.of(context);
+    removeEntry(_entries[key]);
+    _entries[key ?? ValueKey(entry.hashCode)] = entry;
+    _overlay.insert(entry, above: above, below: below);
+  }
+
+  void removeEntry(OverlayEntry entry) {
+    if (entry != null) {
+      _entries.remove(entry);
+      try {
+        entry.remove();
+      } catch (e) {}
+    }
+  }
+
+  void removeKey(Key key) {
+    final entry = _entries[key];
+    if (entry != null) removeEntry(entry);
+  }
+
+  @override
+  bool updateShouldNotify(PortalProvider old) {
+    return entries != old.entries || entries != old.entries;
+  }
+
+  static PortalProvider of(BuildContext context, [String aspect = 'entries']) {
+    return context.dependOnInheritedWidgetOfExactType<PortalProvider>();
+  }
+}
